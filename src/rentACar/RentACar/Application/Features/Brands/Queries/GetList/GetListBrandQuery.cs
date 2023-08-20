55263950eb0439795ffcf7bebb8bed0bdca4b530
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-08-20 20:27:23 +0300

    Commit

---
+++ src/rentACar/RentACar/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Application.Requests;
+
+namespace Application.Features.Brands.Queries.GetList
+{
+    public class GetListBrandQuery
+    {
+        public PageRequests PageRequest { get; set; }
+    }
+}
